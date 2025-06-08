3b3ec135e8849b82d0964b74ae503bb0f381b257
Hieu <hieu250204@gmail.com>
2025-06-08 21:37:12 +0700

    feat:category

---
+++ models/category.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  parent: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category', // tham chiếu đến chính nó
+    default: null,
+  }
+}, {
+  timestamps: true, // thêm createdAt và updatedAt nếu bạn muốn
+});
+
+module.exports = mongoose.model('Category', categorySchema);
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,11 +2,18 @@ const systemConfig = require("../../config/system");
 
 const productRoute = require("./product.route")
 
+const categoryRoute = require("./category.route")
+
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
 
   app.use(
     PATH_ADMIN + "/products",
     productRoute
+  );
+
+  app.use(
+    PATH_ADMIN + "/category",
+    categoryRoute
   );
 }
