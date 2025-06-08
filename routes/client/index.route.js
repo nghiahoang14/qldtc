dde7e72fdf96ed38148957c4fec8f0a6bb4b5bfa
Hieu <hieu250204@gmail.com>
2025-06-08 22:40:17 +0700

    feat: category client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,5 +1,8 @@
 const productRoute = require("./product.route")
 
+const categoryRoute = require("./category.route")
 module.exports = (app) => {
   app.use("/products", productRoute);
+  
+  app.use("/category", categoryRoute);
 }
